0e71ff41dab36a6915ddd672671c528ffd3665b5
TrentWantman <wantmantrent@gmail.com>
2026-03-31 00:36:29 -0400

    Physics model skeleton

---
+++ PhysicsModel.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICSMODEL_H
+#define PHYSICSMODEL_H
+ 
+#include "Vec3.h"
+#include "DoubleCircularBuffer.h"
+ 
+//TODO: Owns the physics (position, velocity, thrust, gravity). Sensors read from this, flight computer sends commands to it.
+ 
+class PhysicsModel {
+ 
+};
+ 
+#endif
